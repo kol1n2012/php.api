e380660c972bffad4383c5a8c0d3a4cc9a6c40f7
kol1n2012 <99890520+kol1n2012@users.noreply.github.com>
2024-04-09 12:40:34 +0300

    +env DEBUG

--- api/index.php
+++ api/index.php
@@ -1,7 +1,9 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+if(filter_var(@getenv('DEBUG'), FILTER_VALIDATE_BOOLEAN)) {
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+}
 
 spl_autoload_register(function ($class_name) {
     $class_name = str_replace('App\\', '', $class_name);
